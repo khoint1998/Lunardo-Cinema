65e5778b6b85fcb102c0f3cd12e353a22c909b47
s3678755 <s3678755@rmit.edu.vn>
2019-10-05 21:44:18 +1000

    test session

--- a4/index.php
+++ a4/index.php
@@ -585,6 +585,7 @@
           </div>
         </fieldset>
       </div>
+      <input type="hidden" name="movie[id]" id="input-movie-id" value=state>
       <input type="hidden" name="movie[id]" id="input-movie-id" value="">
       <input type="hidden" name="movie[day]" id="input-movie-day" value="">
       <input type="hidden" name="movie[hour]" id="input-movie-hour" value="">
