f766904ebf545f64d77046633fd6df4a4d6f7672
s3678755 <s3678755@rmit.edu.vn>
2019-10-09 13:00:44 +1100

    testing csv

--- a4/index.php
+++ a4/index.php
@@ -135,11 +135,11 @@
                   $_SESSION['seats']['FCP']+"\t"+
                   "FCC"+"\t"+
                   $_SESSION['seats']['FCC'];
-                  
+
       $filename = "./bookings.txt";
       $fp = fopen($filename,"a");
       flock($fp, LOCK_EX);
-      fputcsv($fp,"book_str");
+      fputcsv($fp,$_SESSION);
       flock($fp, LOCK_UN);
       fclose($fp);
 
